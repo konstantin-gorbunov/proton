2aa1ae2ab1805874c470d38512a3641a283fb8cf
Kostiantyn Gorbunov <gkboxmail@gmail.com>
2021-10-17 13:44:30 +0200

    - review AppDelegate, optimisation, remove force unwrapping

--- ProtonTest/AppDelegate.swift
+++ ProtonTest/AppDelegate.swift
@@ -9,21 +9,22 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let splitViewController = window!.rootViewController as! UISplitViewController
-        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
-        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
-        splitViewController.delegate = self
+        let splitViewController = window?.rootViewController as? UISplitViewController
+        if let navigationController = splitViewController?.viewControllers.last as? UINavigationController {
+            navigationController.topViewController?.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+        }
+        splitViewController?.delegate = self
         return true
     }
+}
 
+extension AppDelegate: UISplitViewControllerDelegate {
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
         return true
     }
-
 }
-
